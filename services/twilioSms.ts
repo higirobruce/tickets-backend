5503bece3b6fc22ba0915db083cec175a7efea9a
Bruce Higiro <higirobru@gmail.com>
2024-04-21 18:39:46 +0200

    sms fix

--- services/twilioSms.ts
+++ services/twilioSms.ts
@@ -1,5 +1,5 @@
-const accountSid = "AC817ae24cf9b423fe557f8d8892f34b0d";
-const authToken = "49d00428ee53cedac9bf9490fe8efcc7";
+const accountSid = "";
+const authToken = "";
 const client = require("twilio")(accountSid, authToken);
 
 export function twilioSend() {
